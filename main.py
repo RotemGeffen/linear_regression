6b2b53ce7fde3162c7c0b71c4d50b91bc062f0e0
rotem.geffen <rotem.geffen@similarweb.com>
2022-12-28 23:33:15 +0200

    Implement OLD model

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+# imports
+from sklearn.datasets import load_boston
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+from sklearn.model_selection import train_test_split
+import logging
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+X, y = load_boston(return_X_y=True)
+n, p = X.shape
+print(f'p = {p}, n = {n}')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# write a model `Ols` which has a propoery $w$ and 3 methods: `fit`, `predict` and `score`
+class Ols(object):
+    def __init__(self):
+        self.w = None
+
+    @staticmethod
+    def pad(X):
+        return np.hstack([np.ones((X.shape[0], 1)), X])
+
+    def _fit(self, X, Y):
+        # remember pad with 1 before fitting
+        X = Ols.pad(X)
+        self.w = np.linalg.pinv(X.T @ X) @ X.T @ Y
+
+    def _predict(self, X):
+        # return wx
+        return Ols.pad(X) @ self.w
+
+    def score(self, X, Y):
+        return np.mean((self._predict(X)-Y)**2)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+model_ols = Ols()
+model_ols._fit(X, y)
+training_mse = model_ols.score(X, y)
+print(f'The training MSE is: {training_mse}')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
